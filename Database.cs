90a97cebaf1ab87b7e47816231d9a24f72f93d26
Raed Felfel <raedfelfel@outlook.com>
2022-01-06 16:52:56 +0200

    Add project files.

---
+++ Database.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6
+{
+    public class ExamDbContext: DbContext
+    {
+        public ExamDbContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public virtual Category Category { get; set; }
+        [ForeignKey("CatehoryId")]
+        public int CategoryId { get; set; }
+    }
+}
